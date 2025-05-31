0564ddbcc2144d2fb5bf93c91c85e952ffd911c1
tsrikanth2511 <69192814+tsrikanth2511@users.noreply.github.com>
2025-05-31 08:32:20 +0200

    Update home page components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '../components/home/HeroSection';
 import FeaturesSection from '../components/home/FeaturesSection';
-import StatsSection from '../components/home/StatsSection';
 import CallToAction from '../components/home/CallToAction';
 
 const HomePage = () => {
@@ -8,7 +7,6 @@ const HomePage = () => {
     <div>
       <HeroSection />
       <FeaturesSection />
-      <StatsSection />
       <CallToAction />
     </div>
   );
